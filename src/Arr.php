467f33bf74fe4b581f1d1807a3fd28c94ca8f8b4
Vaibhav Kaushal <vaibhavkaushal123@gmail.com>
2018-07-24 17:47:11 +0530

    Added few methods to arr and added the exception class

--- src/Arr.php
+++ src/Arr.php
@@ -8,7 +8,7 @@ namespace Dice\Types ;
 class Arr
 {
 	/**
-	 * @var int Original value with which the class was created
+	 * @var array Original value with which the class was created
 	 */
 	protected $originalValue;
 
@@ -18,10 +18,10 @@ class Arr
 	protected $activeValue;
 
     /**
-     * Integ constructor.
-     * @param String $origValue Original value with which the object is to be constructed
+     * Arr constructor.
+     * @param array $origValue Original value with which the object is to be constructed
      */
-	public function __construct(String $origValue)
+	public function __construct($origValue)
 	{
 		$this->originalValue = $origValue;
 		$this->activeValue = $origValue;
@@ -33,27 +33,71 @@ class Arr
      * @return Arr
      */
 	public static function create($origValue) {
-	    $str = new Integ($origValue);
-	    return $str;
+	    $arr = new Arr($origValue);
+	    return $arr;
     }
 
     /**
-     * String representation of the object
+     * String representation of the array (converted to JSON)
      * @return String returns the active text
      */
 	public function __toString()
 	{
-		return (string)$this->activeValue;
+		return json_encode($this->activeValue);
 	}
 
     /**
-     * @return int Length of the string
+     * Alias of the count function
      */
 	public function length()
 	{
-		return strlen((string)$this->activeValue);
+		return $this->count();
 	}
 
+    /**
+     * @return int Number of items in the array
+     */
+	public function count() {
+        return count($this->activeValue);
+    }
 
+    /**
+     * Puts an item at the end of the array for numeric indexes.
+     *
+     * Use addIndexedItem method to add an item with an index.
+     *
+     * @param mixed $item Item to be pushed to the array
+     */
+    public function append($item) {
+	    array_push($this->activeValue, $item);
+    }
+
+    /**
+     * Puts an item at the end of the array for numeric indexes.
+     *
+     * Use addIndexedItem method to add an item with an index.
+     *
+     * @param mixed $item Item to be pushed to the array
+     * @throws Exception\InvalidTypeException
+     */
+    public function prepend($item) {
+        if (is_resource($item)) {
+            throw new Exception\InvalidTypeException('You cannot add a resource to an array.');
+        }
+
+        if (is_array($item)) {
+            throw new Exception\InvalidTypeException('You cannot prepend an array to an array as of now.');
+        }
+
+        array_unshift($this->activeValue, $item);
+    }
+
+    /**
+     * @param string $index The array index to be used when setting the item
+     * @param mixed $item Item to be pushed to the array
+     */
+    public function addIndexedItem($index, $item) {
+        $this->activeValue[$index] = $item;
+    }
 
 }
---
+++ src/Exception/InvalidTypeException.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * If any method is trying to work with invalid/incompatible types, this exception has to be thrown
+ */
+
+namespace Dice\Types\Exception;
+
+class InvalidTypeException extends \Exception {
+
+}
