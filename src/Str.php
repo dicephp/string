5995992e78889b118d790bba207ded435f1e20b7
Vaibhav Kaushal <vaibhavkaushal123@gmail.com>
2018-07-24 16:44:37 +0530

    Added version

--- src/Str.php
+++ src/Str.php
@@ -21,7 +21,7 @@ class Str
      * Str constructor.
      * @param String $origText Original text with which the object is to be constructed
      */
-	public function __construct(String $origText)
+	public function __construct(Str $origText)
 	{
 		$this->originalText = $origText;
 		$this->activeText = $origText;
@@ -32,7 +32,7 @@ class Str
      * @param String $origText
      * @return Str
      */
-	public static function create(String $origText) {
+	public static function create(Str $origText) {
 	    $str = new Str($origText);
 	    return $str;
     }
@@ -396,30 +396,3 @@ class Str
 
 }
 
-$obj = new Str(" Vaibhav Kaushal ");
-//echo "\nlength: " . $obj->length();
-//echo "\nstring cast: '" . $obj . "'";
-//echo "\ntrim: '" . $obj->trim() . "'";;
-//echo "\nlength after trimming: " . $obj->length();
-//echo "\nbefore reset: '" . $obj . "'";
-//echo "\ncalling reset: '" . $obj->reset() . "'";
-//echo "\ntrimming again: '" . $obj->trim() . "'";
-//echo "\ntoggle case: '" . $obj->togglecase() . "'";
-//echo "\ntoggle case again: '" . $obj->togglecase() . "'";
-//echo "\ndowncasing: '" . $obj->downcase() . "'";
-//echo "\nupcasing: '" . $obj->upcase() . "'";
-//echo "\ncalling reset: '" . $obj->reset() . "'";
-//echo "\ncalling chain (trim-upcase-togglecase): '" . $obj->trim()->upcase()->togglecase() . "'";
-//
-//echo "\n============";
-//echo "\ncalling trim again: '" . $obj->trim() . "'";
-//echo "\ncalling append: '" . $obj->append(' is a good boy') . "'";
-//echo "\ncalling prepend: '" . $obj->prepend('Actually, ') . "'";
-//
-//echo "\n============";
-//echo "\ncalling reset: '" . $obj->reset() . "'";
-echo "\ncalling ljust: '" . $obj->lJust(25, '1234567890') . "'";
-$obj->reset();
-echo "\ncalling rjust: '" . $obj->rJust(25, '1234567890') . "'";
-
-echo "\n";
